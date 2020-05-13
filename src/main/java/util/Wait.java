a1d60f301bf1a0f34264f67c9846ba2b0880d8d3
MDTA <anas.yaghi@transcore.com>
2020-05-13 09:01:39 -0500

    updating

--- src/main/java/util/Wait.java
+++ src/main/java/util/Wait.java
@@ -8,12 +8,12 @@ package util;
 	import org.openqa.selenium.support.ui.ExpectedConditions;
 	import org.openqa.selenium.support.ui.WebDriverWait;
 
-	public class Wait {
-		public static void waitForElement(WebDriver driver , int timeInSeconds , By Locator ) {
-			WebDriverWait wait = new WebDriverWait(driver , timeInSeconds);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(Locator));
-		}
+//	public static class Wait {
+//		public  void waitForElement(WebDriver driver , int timeInSeconds , By Locator ) {
+//			WebDriverWait wait = new WebDriverWait(driver , timeInSeconds);
+//			wait.until(ExpectedConditions.visibilityOfElementLocated(Locator));
+//		}
 
-	}
+//	}
 
 
--- src/test/java/test/LAMetro_Login_Test.java
+++ src/test/java/test/LAMetro_Login_Test.java
@@ -2,19 +2,24 @@ package test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import util.OpenBrowser;
-import util.Wait;
+
+
+
+
+
 
 public class LAMetro_Login_Test {
 	WebDriver driver;
 	
 	@BeforeTest
 	public void LoginPage() {
-		driver = OpenBrowser.openBrowser("https://www.metroexpresslanes.net/");
+		driver.get("https://www.metroexpresslanes.net/");
 
 	}
 	
@@ -22,13 +27,24 @@ public class LAMetro_Login_Test {
 	public void Login() throws InterruptedException {
 		
 		driver.findElement(By.xpath("//span/i")).click();
-		Wait.waitForElement(driver, 20, By.xpath("//ul[@id='menu-top-menu']/li/a[@title='Login']"));
+		
+		//Wait.waitForElement(driver, 20, By.xpath("//ul[@id='menu-top-menu']/li/a[@title='Login']"));
+		
+		WebDriverWait wait = new WebDriverWait(driver , 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@id='menu-top-menu']/li/a[@title='Login']")));
 		driver.findElement(By.xpath("//ul[@id='menu-top-menu']/li/a[@title='Login']")).click();
-		Wait.waitForElement(driver, 20, By.id("input_2"));
+		
+		//Wait.waitForElement(driver, 20, By.id("input_2"));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input_2")));
+		
 		driver.findElement(By.id("input_2")).sendKeys("KerrSmoke");
 		driver.findElement(By.id("input_3")).sendKeys("S@ndieg0");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		Wait.waitForElement(driver, 20, By.xpath("//div[text()='Summary']"));
+		
+	//	Wait.waitForElement(driver, 20, By.xpath("//div[text()='Summary']"));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Summary']")));
 	}
 	
 	
@@ -36,7 +52,12 @@ public class LAMetro_Login_Test {
 	public void LogOut() throws InterruptedException {
 		
 		driver.findElement(By.xpath("//span[@ng-bind='authentication.userName']")).click();
-		Wait.waitForElement(driver, 20, By.xpath("//span[text()='Logout']"));
+		
+		//Wait.waitForElement(driver, 20, By.xpath("//span[text()='Logout']"));
+		
+		WebDriverWait wait2 = new WebDriverWait(driver , 20);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Logout']")));
+		
 		driver.findElement(By.xpath("//span[text()='Logout']")).click();
 }
 	
